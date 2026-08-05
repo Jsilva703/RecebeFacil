0e9c2a78874dcdf7d6d13ba001433d00a0ec618e
Jsilva703 <joaovictornunessilva12345@gmail.com>
2026-08-05 10:39:45 -0300

    add username validation and normalization to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,24 @@
 class User < ApplicationRecord
   STATUSES = %w[active inactive suspended].freeze
-  belongs_to :account
 
-  before_validation :normalize_email
+  USERNAME_FORMAT =     /\A(?=.*[a-z])[a-z0-9](?:[a-z0-9._]*[a-z0-9])?\z/
+
+  belongs_to :account
+  before_validation :normalize_email, :normalize_username
 
   validates :account, presence: true
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :status, presence: true, inclusion: { in: STATUSES }
+  validates :username, presence: true, length: { in: 3..30}, format: { with: USERNAME_FORMAT }, uniqueness: { case_sensitive: false }
 
   private
 
   def normalize_email
     self.email = email.to_s.strip.downcase if email.present?
+  end
+
+  def normalize_username
+    self.username = username.to_s.strip.downcase if username.present?
   end
 end
